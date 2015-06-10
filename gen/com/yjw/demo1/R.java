880e87bc7b6d3387052b24553f95762271a9f616
Highter <yjwsmileatlife@126.com>
2015-06-11 01:18:02 +0800

    Button事件监听三种方法

--- gen/com/yjw/demo1/R.java
+++ gen/com/yjw/demo1/R.java
@@ -24,12 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080005;
-        public static final int editText1=0x7f080003;
-        public static final int imageView1=0x7f080004;
-        public static final int myBtn=0x7f080000;
-        public static final int myImageBtn=0x7f080001;
-        public static final int textView1=0x7f080002;
+        public static final int action_settings=0x7f080006;
+        public static final int editText1=0x7f080004;
+        public static final int imageView1=0x7f080005;
+        public static final int myBtn1=0x7f080000;
+        public static final int myBtn2=0x7f080001;
+        public static final int myBtn3=0x7f080002;
+        public static final int textView1=0x7f080003;
     }
     public static final class layout {
         public static final int activity_button=0x7f030000;
--- src/com/yjw/demo1/ButtonListener.java
+++ src/com/yjw/demo1/ButtonListener.java
@@ -2,12 +2,67 @@ package com.yjw.demo1;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class ButtonListener extends Activity{
+public class ButtonListener extends Activity implements OnClickListener{
+
+	private Button myButton1;
+	private Button myButton2;
+	private Button myButton3;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_button);
+
+		// findViewById:返回一个View对象
+		myButton1 = (Button) findViewById(R.id.myBtn1);
+		/*
+		 * 监听事件实现方式 :1、匿名内部类； 2、外部类实现； 3、接口方式实现
+		 */
+		myButton1.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Log.i("tag","匿名类实现");
+			}
+		});
+
+		myButton2 = (Button) findViewById(R.id.myBtn2);
+		myButton2.setOnClickListener(new MyOnclickListener(){
+			@Override
+			public void onClick(View v) {
+				// 调用父类的onClick()方法
+				super.onClick(v);
+				Toast.makeText(ButtonListener.this, "外部类实现", 1).show();
+			}
+		});
+		
+		myButton3 = (Button) findViewById(R.id.myBtn3);
+		myButton3.setOnClickListener(this);
+	}
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Log.i("tag","接口方式实现");
+	}
+
+}
+
+class MyOnclickListener implements OnClickListener {
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Log.i("tag", "外部类实现");
+		//改变透明度
+		v.setAlpha(0.5f);
 	}
 }
