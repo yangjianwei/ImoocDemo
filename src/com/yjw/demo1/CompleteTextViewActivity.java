66b7f2451cf8961cf9872c32d744e7af6ff5313a
Higher <yjwsmileatlife@126.com>
2015-06-11 14:54:13 +0800

    准备实现动态自动匹配输入的内容

---
+++ src/com/yjw/demo1/CompleteTextViewActivity.java
@@ -0,0 +1,12 @@
+package com.yjw.demo1;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CompleteTextViewActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
